636444fdc7af2f5914bfbf8bd0e520506be86acc
dpjollygreen <149906922+dpjollygreen@users.noreply.github.com>
2024-07-01 11:02:59 -0700

    Revert "Lesson 5 Phrase guess game"

--- MultipleLettersException.java
+++
@@ -1,7 +0,0 @@
-public class MultipleLettersException extends Exception{
-    @Override
-    public String getMessage() {
-        //System.out.println("More than one letter was entered");
-        return "More than one letter was entered";
-    }
-}
